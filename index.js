4ee553f61ddc533f2e897390cf26a00a151a8b4a
jukyg <safi2423f@gmail.com>
2026-02-26 16:04:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ app.listen(process.env.PORT || 3000);
 client.on('ready', async () => {
   console.log(`✅ SUCCESS: Logged in as ${client.user.tag}`);
   
-  // 60 ساعة = 2.5 يوم
   const sixtyHoursAgo = Date.now() - (60 * 60 * 60 * 1000);
   
   const r = new Discord.RichPresence()
     .setApplicationId('1476320950323642478')
     .setType('PLAYING')
     .setName('saif ₉₁')
-    .setDetails('ME... 🚗')
+    .setDetails('ME... ')
     .setState('Watching ˢᵃᶤᶠ ₉₁')
     .setStartTimestamp(sixtyHoursAgo)
     .addButton('ME ?', 'https://linktr.ee/povce')
     .addButton('server', 'https://discord.gg/3HzTN5rv');
   
   client.user.setActivity(r);
-  client.user.setStatus('dnd');
-  console.log('✅ Presence active - Timer shows 60:00:00 (2.5 days)');
+  // شلنا client.user.setStatus() خالص!
+  
+  console.log('✅ Presence active - using your manual status!');
 });
 
 client.login(process.env.TOKEN);
